f25aa75cc986c4fe81085e1df43b70b55159556f
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2024-02-26 12:31:25 -0300

    Calculadora 1.8

---
+++ JavaScript/script.js
@@ -0,0 +1,31 @@
+// [].push para adicionar um elemento em um array
+// [].pop para remover
+let res = document.querySelector('div#res');
+
+function Inserir(valor){
+    
+    const ultimo = res.innerHTML.slice(-1);
+    if(Operador(ultimo) && Operador(valor)){
+        // Se os dois forem operadores, substitui um pelo outro;
+        res.innerHTML = res.innerHTML.slice(0, -1) + valor;
+    } else {
+        res.innerHTML += valor;
+    }
+    
+}
+function Operador(op){
+    return ['+', '-', '*', '/', '%'].includes(op);
+}
+
+function result(){
+    res.innerHTML = eval(res.innerHTML);
+}
+
+function limpar(){
+    res.innerHTML = '';
+}
+
+function voltar(){
+    let resultado = res.innerHTML;
+    res.innerHTML = resultado.substring(0, resultado.length -1);
+}
